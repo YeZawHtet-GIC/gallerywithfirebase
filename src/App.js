c69f57deaa389c83a2ec88029bf6d318a1a04a2a
YeZawHtet-GIC <yezawhtet@gicjp.com>
2024-03-14 09:04:23 +0630

    fix enlarge image style

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { v4 } from "uuid";
 function App() {
   const [imagesUpload, setImagesUpload] = useState([]);
   const [imageList, setImageList] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null); // New state to track selected image
   const imageListRef = ref(storage, "images/");
 
   const handleFileChange = (event) => {
@@ -19,8 +20,8 @@ function App() {
 
     imagesUpload.forEach((image) => {
       const storageRef = ref(storage, `images/${image.name + v4()}`);
-      uploadBytes(storageRef, image).then((snaphsot) => {
-        getDownloadURL(snaphsot.ref).then((url) => {
+      uploadBytes(storageRef, image).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
           setImageList((prev) => [...prev, url]);
         });
       });
@@ -37,40 +38,74 @@ function App() {
     });
   }, []);
 
+  const enlargeImage = (url) => {
+    setSelectedImage(url);
+  };
+
+  const closeEnlargeImage = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <div className="col-md-10 offset-1">
-      <div className="col-md-6 offset-2">
-        <input
-          type="file"
-          className="form-control mt-5"
-          onChange={handleFileChange}
-          multiple // Allow multiple file selection
-        />
-        <button className="my-3 btn btn-outline-success" onClick={uploadImages}>
-          Upload Images
-        </button>
-      </div>
-      <div className="row mt-3">
-        <div className="col-md-10 offset-1">
-          {imageList &&
-            imageList.map((url) => {
-              return (
-                <img
-                  key={url} // Add a unique key for each image
-                  style={{
-                    width: "200px",
-                    height: "200px",
-                    objectFit: "cover",
-                  }}
-                  src={url}
-                  alt={url}
-                  className="m-3 rounded img-fluid img-thumbnail"
-                />
-              );
-            })}
+    <>
+      <div className="col-md-10 offset-1">
+        <div className="col-md-6 offset-2">
+          <input
+            type="file"
+            className="form-control mt-5"
+            onChange={handleFileChange}
+            multiple // Allow multiple file selection
+          />
+          <button
+            className="my-3 btn btn-outline-success"
+            onClick={uploadImages}
+          >
+            Upload Images
+          </button>
+        </div>
+        <div className="row mt-3">
+          <div className="col-md-10 offset-1">
+            {imageList &&
+              imageList.map((url) => {
+                return (
+                  <img
+                    key={url} // Add a unique key for each image
+                    style={{
+                      width: "200px",
+                      height: "200px",
+                      objectFit: "cover",
+                      cursor: "pointer", // Add pointer cursor for clickable images
+                    }}
+                    src={url}
+                    alt={url}
+                    className="m-3 rounded img-fluid img-thumbnail"
+                    onClick={() => enlargeImage(url)} // Call enlargeImage function on click
+                  />
+                );
+              })}
+          </div>
         </div>
       </div>
-    </div>
+      {/* Conditionally render enlarged image with overlay */}
+      {selectedImage && (
+        <div className="enlarged-container">
+          <div
+            className="enlarged-background"
+            onClick={closeEnlargeImage}
+          ></div>
+          <div className="enlarged-content">
+            <img
+              src={selectedImage}
+              alt="Enlarged"
+              className="enlarged-image"
+            />
+            <button className="close-button btn btn-danger fw-bold" onClick={closeEnlargeImage}>
+              X
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
